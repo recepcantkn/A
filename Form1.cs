d957e8987d462fe81017256d991e25db47f1f65e
recepcantkn <61096668+recepcantkn@users.noreply.github.com>
2022-03-01 09:00:41 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ArrayList_form_tasarımı_uygulaması_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList sehirler = new ArrayList();
+
+        private void Listele()
+        {
+            listeSehirler.Items.Clear();
+            foreach (string sehir in sehirler)
+            {
+                listeSehirler.Items.Add(sehir);
+            }
+        }
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            sehirler.Add(txtSehirler.Text);
+            Listele();
+        }
+
+        private void btnAraEkle_Click(object sender, EventArgs e)
+        {
+            int indexNo = listeSehirler.SelectedIndex;
+            sehirler.Insert(indexNo, txtSehirler.Text);
+            Listele();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            int indexNo = listeSehirler.SelectedIndex;
+            sehirler[indexNo] = txtSehirler.Text;
+            Listele();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int indexNo = listeSehirler.SelectedIndex;
+            sehirler.RemoveAt(indexNo);
+            Listele();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            if (sehirler.Contains(txtSehirler.Text))
+        {
+        labelDurum.Text = "Aranan Değer Bulundu.";
+        listeSehirler.SelectedIndex = sehirler.IndexOf(txtSehirler.Text);
+        }
+        else
+        {
+        labelDurum.Text = "Aranan Değer Bulunamadı.";
+        }
+         } 
+        }
+       }
+
